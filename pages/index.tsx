b677690696c1c8b4327adb7b3634660239fb9354
tharunoptimus <tharunoptimus@outlook.com>
2023-01-28 17:47:53 +0530

    Added favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ function IndexPage(): JSX.Element {
 	return (
 		<>
 			<Head>
+				<link rel="apple-touch-icon" href="./apple-touch-icon.png" />
 				<meta property="descript" content="Index Page" />
+				<title>Gold One Refinery Private Limited</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
 			</Head>
 			<p>This is the index page</p>
 		</>
