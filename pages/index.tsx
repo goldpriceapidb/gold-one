4b8de1773a0a16d4a7a0f3d2a9b3ffa5dfedceea
tharunoptimus <tharunoptimus@outlook.com>
2023-01-28 17:48:29 +0530

    Added global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,35 @@ function IndexPage(): JSX.Element {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
 			</Head>
-			<p>This is the index page</p>
+			<style global jsx>{`
+				@font-face {
+					font-family: "Bahnschrift";
+					src: url("fonts/Bahnschrift.eot");
+					src: local("Bahnschrift"),
+						url("fonts/Bahnschrift.eot?#iefix")
+							format("embedded-opentype"),
+						url("fonts/Bahnschrift.woff2") format("woff2"),
+						url("fonts/Bahnschrift.woff") format("woff"),
+						url("fonts/Bahnschrift.ttf") format("truetype"),
+						url("fonts/Bahnschrift.svg#Bahnschrift") format("svg");
+					font-weight: normal;
+					font-style: normal;
+					font-display: swap;
+				}
+
+				* {
+					font-family: "Bahnschrift", system-ui, -apple-system,
+						BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu,
+						Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+					font-weight: 400;
+				}
+
+				body {
+					margin: 0;
+					padding: 0;
+					background-color: #000000;
+				}
+			`}</style>
 		</>
 	)
 }
