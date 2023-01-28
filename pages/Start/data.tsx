d0eaf0eabdc89e24674d836e530a8c3994136457
tharunoptimus <tharunoptimus@outlook.com>
2023-01-28 19:18:51 +0530

    Added country name and code

--- pages/Start/data.tsx
+++ pages/Start/data.tsx
@@ -9,4 +9,207 @@ let quote: QuoteType = {
 	saidBy: "JP Morgan",
 }
 
-export { quote }
+let countries = [
+	{
+		countryName: "Malaysia",
+		countryCode: "malaysia"
+	},
+	{
+		countryName: "Dubai",
+		countryCode: "dubai"
+	},
+	{
+		countryName: "Turkey",
+		countryCode: "turkey"
+	},
+	{
+		countryName: "Philippines",
+		countryCode: "philippines"
+	},
+	{
+		countryName: "India",
+		countryCode: "india"
+	},
+	{
+		countryName: "Argentina",
+		countryCode: "argentina"
+	},
+	{
+		countryName: "Ecuador",
+		countryCode: "ecuador"
+	},
+	{
+		countryName: "Brazil",
+		countryCode: "brazil"
+	},
+	{
+		countryName: "Canada",
+		countryCode: "canada"
+	},
+	{
+		countryName: "Peru",
+		countryCode: "peru"
+	},
+	{
+		countryName: "Finland",
+		countryCode: "finland"
+	},
+	{
+		countryName: "Madagascar",
+		countryCode: "madagascar"
+	},
+	{
+		countryName: "Sweden",
+		countryCode: "sweden"
+	},
+	{
+		countryName: "Mexico",
+		countryCode: "mexico"
+	},
+	{
+		countryName: "Sudan",
+		countryCode: "sudan"
+	},
+	{
+		countryName: "Russia",
+		countryCode: "russia"
+	},
+	{
+		countryName: "Ghana",
+		countryCode: "ghana"
+	},
+	{
+		countryName: "Uzbekistan",
+		countryCode: "uzbekistan"
+	},
+	{
+		countryName: "Chile",
+		countryCode: "chile"
+	},
+	{
+		countryName: "Bulgaria",
+		countryCode: "bulgaria"
+	},
+	{
+		countryName: "Tanzania",
+		countryCode: "tanzania"
+	},
+	{
+		countryName: "Indonesia",
+		countryCode: "indonesia"
+	},
+	{
+		countryName: "Australia",
+		countryCode: "australia"
+	},
+	{
+		countryName: "Colombia",
+		countryCode: "colombia"
+	},
+	{
+		countryName: "Venezuela",
+		countryCode: "venezuela"
+	},
+	{
+		countryName: "Zimbabwe",
+		countryCode: "zimbabwe"
+	},
+	{
+		countryName: "Mali",
+		countryCode: "mali"
+	},
+	{
+		countryName: "Mauritania",
+		countryCode: "mauritania"
+	},
+	{
+		countryName: "Niger",
+		countryCode: "niger"
+	},
+	{
+		countryName: "Senegal",
+		countryCode: "senegal"
+	},
+	{
+		countryName: "Singapore",
+		countryCode: "singapore"
+	},
+	{
+		countryName: "Laos",
+		countryCode: "laos"
+	},
+	{
+		countryName: "China",
+		countryCode: "china"
+	},
+	{
+		countryName: "Kazakhstan",
+		countryCode: "kazakhstan"
+	},
+	{
+		countryName: "Guinea",
+		countryCode: "guinea"
+	},
+	{
+		countryName: "Bolivia",
+		countryCode: "bolivia"
+	},
+	{
+		countryName: "United States",
+		countryCode: "usa"
+	},
+	{
+		countryName: "Dominican Republic",
+		countryCode: "dominican-republic"
+	},
+	{
+		countryName: "Burkina Faso",
+		countryCode: "burkina-faso"
+	},
+	{
+		countryName: "South Africa",
+		countryCode: "south-africa"
+	},
+	{
+		countryName: "DR Congo",
+		countryCode: "congo"
+	},
+	{
+		countryName: "Ivory Coast",
+		countryCode: "cote-divoire"
+	},
+	{
+		countryName: "Papua New Guinea",
+		countryCode: "papua-new-guinea"
+	},
+	{
+		countryName: "New Zealand",
+		countryCode: "new-zealand"
+	},
+	{
+		countryName: "LBMA",
+		countryCode: "united-kingdom"
+	},
+	{
+		countryName: "Guyana",
+		countryCode: "gy"
+	},
+	{
+		countryName: "Liberia",
+		countryCode: "lr"
+	},
+	{
+		countryName: "Mongolia",
+		countryCode: "mn"
+	},
+	{
+		countryName: "Kyrgyzstan",
+		countryCode: "kg"
+	},
+	{
+		countryName: "Suriname",
+		countryCode: "sr"
+	},
+]
+
+export { quote, countries }
