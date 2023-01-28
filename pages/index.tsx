ff49e6924b81ba44774f0e8ae61c1243b9361a21
tharunoptimus <tharunoptimus@outlook.com>
2023-01-28 16:02:33 +0530

    Refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,14 @@
-import Head from 'next/head'
-import { GetStaticProps, NextPage } from 'next'
-import Post from '../components/post'
-import { PostData, PostDataListProps } from '../types/postdata'
-import { GetPosts } from '../lib/postdata_api'
+import Head from "next/head"
 
-export const getStaticProps: GetStaticProps = async (_context) => {
-  // fetch list of posts
-  const posts: PostData[] = await GetPosts()
-  return {
-    props: {
-      postDataList: posts,
-    },
-  }
-}
-
-const IndexPage: NextPage<PostDataListProps> = ({
-  postDataList,
-}: PostDataListProps) => {
-  return (
-    <main>
-      <Head>
-        <title>Home page</title>
-      </Head>
-
-      <h1>List of posts</h1>
-
-      <section>
-        {postDataList.map((post: PostData) => (
-          <Post {...post} key={post.id} />
-        ))}
-      </section>
-    </main>
-  )
+function IndexPage(): JSX.Element {
+	return (
+		<>
+			<Head>
+				<meta property="descript" content="Index Page" />
+			</Head>
+			<p>This is the index page</p>
+		</>
+	)
 }
 
 export default IndexPage
