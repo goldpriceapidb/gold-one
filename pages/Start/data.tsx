e141424aba2a2ab8b6ec657f186401d2322c5516
tharunoptimus <tharunoptimus@outlook.com>
2023-01-31 09:26:44 +0530

    Moved india to the top of the array, and added random key to the entries

--- pages/Start/data.tsx
+++ pages/Start/data.tsx
@@ -1,4 +1,4 @@
-import { QuoteType } from "../../types/QuoteTypes"
+import { QuoteType } from "../../types/StartPageTypes"
 
 export default function Data(): JSX.Element {
 	return <></>
@@ -11,204 +11,254 @@ let quote: QuoteType = {
 
 let countries = [
 	{
+		countryName: "India",
+		countryCode: "india",
+		key: "7d8aef20-06e9-418c-a977-b96423ed8324"
+	},
+	{
 		countryName: "Malaysia",
-		countryCode: "malaysia"
+		countryCode: "malaysia",
+		key: "e9d42c55-12a1-4b74-bc1e-e61153243650"
 	},
 	{
 		countryName: "Dubai",
-		countryCode: "dubai"
+		countryCode: "dubai",
+		key: "4b1a7733-8a96-4fed-ad80-5d7d6f8010bf"
 	},
 	{
 		countryName: "Turkey",
-		countryCode: "turkey"
+		countryCode: "turkey",
+		key: "626d721c-fedc-4fcd-b663-34ae46e8b429"
 	},
 	{
 		countryName: "Philippines",
-		countryCode: "philippines"
-	},
-	{
-		countryName: "India",
-		countryCode: "india"
+		countryCode: "philippines",
+		key: "7debd9ac-3e68-4458-8100-3947509ac854"
 	},
 	{
 		countryName: "Argentina",
-		countryCode: "argentina"
+		countryCode: "argentina",
+		key: "a861dc9d-ab24-4887-90b4-611012b837a8"
 	},
 	{
 		countryName: "Ecuador",
-		countryCode: "ecuador"
+		countryCode: "ecuador",
+		key: "cef26039-5b2c-478c-beee-6861a45a3b48"
 	},
 	{
 		countryName: "Brazil",
-		countryCode: "brazil"
+		countryCode: "brazil",
+		key: "4d816738-9f79-433d-b3d3-99a2cd43a2cf"
 	},
 	{
 		countryName: "Canada",
-		countryCode: "canada"
+		countryCode: "canada",
+		key: "4ab8fa7e-8537-4037-b111-38819812b7ef"
 	},
 	{
 		countryName: "Peru",
-		countryCode: "peru"
+		countryCode: "peru",
+		key: "4bd3adfc-21a8-431b-9aa9-7114c05046e4"
 	},
 	{
 		countryName: "Finland",
-		countryCode: "finland"
+		countryCode: "finland",
+		key: "7a63ab19-8306-4c72-97d5-7e218bbdded2"
 	},
 	{
 		countryName: "Madagascar",
-		countryCode: "madagascar"
+		countryCode: "madagascar",
+		key: "ef7233f7-8bb1-4e11-a027-a57ac9c7c04e"
 	},
 	{
 		countryName: "Sweden",
-		countryCode: "sweden"
+		countryCode: "sweden",
+		key: "0ea7a29f-9fac-4fe3-b1af-919867b6b31c"
 	},
 	{
 		countryName: "Mexico",
-		countryCode: "mexico"
+		countryCode: "mexico",
+		key: "30dd4875-7d3c-48d3-9ac9-c522aa83dd12"
 	},
 	{
 		countryName: "Sudan",
-		countryCode: "sudan"
+		countryCode: "sudan",
+		key: "df357206-cdbe-4fc4-9810-901eef092974"
 	},
 	{
 		countryName: "Russia",
-		countryCode: "russia"
+		countryCode: "russia",
+		key: "f67ce858-d0f2-4e9d-8ad3-81ecf0b268d2"
 	},
 	{
 		countryName: "Ghana",
-		countryCode: "ghana"
+		countryCode: "ghana",
+		key: "0b9e9add-0981-487c-91e0-1133c82ca82f"
 	},
 	{
 		countryName: "Uzbekistan",
-		countryCode: "uzbekistan"
+		countryCode: "uzbekistan",
+		key: "c4f290ce-5514-4266-96a5-a8dea657e0c7"
 	},
 	{
 		countryName: "Chile",
-		countryCode: "chile"
+		countryCode: "chile",
+		key: "7521a88a-06e8-475b-8082-6360bf3569f9"
 	},
 	{
 		countryName: "Bulgaria",
-		countryCode: "bulgaria"
+		countryCode: "bulgaria",
+		key: "d6649c85-1faf-4958-a768-8cf6585b2caa"
 	},
 	{
 		countryName: "Tanzania",
-		countryCode: "tanzania"
+		countryCode: "tanzania",
+		key: "d7d55a06-f9a5-41fa-bf89-9d5b862f965f"
 	},
 	{
 		countryName: "Indonesia",
-		countryCode: "indonesia"
+		countryCode: "indonesia",
+		key: "1a6bd4bb-b561-4d90-9dbb-d24448b826c8"
 	},
 	{
 		countryName: "Australia",
-		countryCode: "australia"
+		countryCode: "australia",
+		key: "3e1d70a8-7412-4732-965a-40d7bba40dfd"
 	},
 	{
 		countryName: "Colombia",
-		countryCode: "colombia"
+		countryCode: "colombia",
+		key: "6270db88-0ff9-4a54-80ed-612eb444e659"
 	},
 	{
 		countryName: "Venezuela",
-		countryCode: "venezuela"
+		countryCode: "venezuela",
+		key: "ee45bfb0-f030-4df5-97cd-423e401c361b"
 	},
 	{
 		countryName: "Zimbabwe",
-		countryCode: "zimbabwe"
+		countryCode: "zimbabwe",
+		key: "c732b785-e0d0-4dbf-afed-c6b5b030da2f"
 	},
 	{
 		countryName: "Mali",
-		countryCode: "mali"
+		countryCode: "mali",
+		key: "ffa3494f-ac11-4d26-8541-4e9c5a0ac52a"
 	},
 	{
 		countryName: "Mauritania",
-		countryCode: "mauritania"
+		countryCode: "mauritania",
+		key: "e1eb0197-ba9e-4764-b50e-180660295dc4"
 	},
 	{
 		countryName: "Niger",
-		countryCode: "niger"
+		countryCode: "niger",
+		key: "54623672-7ec7-482b-a864-04e2d5722c6c"
 	},
 	{
 		countryName: "Senegal",
-		countryCode: "senegal"
+		countryCode: "senegal",
+		key: "493c71c0-a788-43f6-963c-628a30753d29"
 	},
 	{
 		countryName: "Singapore",
-		countryCode: "singapore"
+		countryCode: "singapore",
+		key: "0cdc05fd-44ea-4320-9ff2-e924bd8049b5"
 	},
 	{
 		countryName: "Laos",
-		countryCode: "laos"
+		countryCode: "laos",
+		key: "ce3b10e9-69a2-4c16-bb5b-debde6d1a40d"
 	},
 	{
 		countryName: "China",
-		countryCode: "china"
+		countryCode: "china",
+		key: "6967690a-7ba2-4522-b1b5-967d1881b6e7"
 	},
 	{
 		countryName: "Kazakhstan",
-		countryCode: "kazakhstan"
+		countryCode: "kazakhstan",
+		key: "9a2d9d6e-cd11-43e7-b7a1-42860b24222e"
 	},
 	{
 		countryName: "Guinea",
-		countryCode: "guinea"
+		countryCode: "guinea",
+		key: "f17d421d-f273-47d1-9175-97deade3940c"
 	},
 	{
 		countryName: "Bolivia",
-		countryCode: "bolivia"
+		countryCode: "bolivia",
+		key: "194a84bf-4cc9-44a1-8f2a-b23760de2291"
 	},
 	{
 		countryName: "United States",
-		countryCode: "usa"
+		countryCode: "usa",
+		key: "69ecd5b2-9bcb-4cae-a8e4-cb1df9262cd6"
 	},
 	{
 		countryName: "Dominican Republic",
-		countryCode: "dominican-republic"
+		countryCode: "dominican-republic",
+		key: "336331fb-0809-46e9-998c-27b89f7b911f"
 	},
 	{
 		countryName: "Burkina Faso",
-		countryCode: "burkina-faso"
+		countryCode: "burkina-faso",
+		key: "42b60766-1ed9-4d8a-b38a-fb7f6de4955a"
 	},
 	{
 		countryName: "South Africa",
-		countryCode: "south-africa"
+		countryCode: "south-africa",
+		key: "32569832-e809-4eb3-bf55-68088f9e1b89"
 	},
 	{
 		countryName: "DR Congo",
-		countryCode: "congo"
+		countryCode: "congo",
+		key: "e73e734c-1a6c-48f7-8895-a150e72d7f2b"
 	},
 	{
 		countryName: "Ivory Coast",
-		countryCode: "cote-divoire"
+		countryCode: "cote-divoire",
+		key: "abd97e1f-ba2d-4206-a38a-2fdfa5d1636d"
 	},
 	{
 		countryName: "Papua New Guinea",
-		countryCode: "papua-new-guinea"
+		countryCode: "papua-new-guinea",
+		key: "a6bd150d-fe4e-42a8-bf27-675ed94a0cb7"
 	},
 	{
 		countryName: "New Zealand",
-		countryCode: "new-zealand"
+		countryCode: "new-zealand",
+		key: "ef04ac6f-42fc-4831-9a26-e997509c7304"
 	},
 	{
 		countryName: "LBMA",
-		countryCode: "united-kingdom"
+		countryCode: "united-kingdom",
+		key: "35f86371-5af5-43de-8487-2722c0eeba74"
 	},
 	{
 		countryName: "Guyana",
-		countryCode: "gy"
+		countryCode: "gy",
+		key: "668288b5-aed3-4a17-a5f7-f5ab291a18e2"
 	},
 	{
 		countryName: "Liberia",
-		countryCode: "lr"
+		countryCode: "lr",
+		key: "0b51d3d3-673e-4064-a05b-88f4fcfa7fc7"
 	},
 	{
 		countryName: "Mongolia",
-		countryCode: "mn"
+		countryCode: "mn",
+		key: "9206a65e-cbff-4f23-ae4e-d3e06cc85f91"
 	},
 	{
 		countryName: "Kyrgyzstan",
-		countryCode: "kg"
+		countryCode: "kg",
+		key: "ca32e61d-1d9b-40a7-81f3-9db5ace19a3e"
 	},
 	{
 		countryName: "Suriname",
-		countryCode: "sr"
+		countryCode: "sr",
+		key: "359ea6dc-aa73-4cdc-9c08-03c23a5f16ce"
 	},
 ]
 
