72d0e700e41ab21b8572012fb115f2485b0d4bd6
tharunoptimus <tharunoptimus@outlook.com>
2023-01-31 11:53:03 +0530

    Added `updateValues`

--- pages/Start/components/LiveSpotPrice.tsx
+++ pages/Start/components/LiveSpotPrice.tsx
@@ -1,3 +1,10 @@
+import {
+	CountryType,
+	CountrySelect,
+	KaratSelect,
+	Country,
+	RenderValue,
+} from "../../../types/StartPageTypes"
 export default function LiveSpotPrice(): JSX.Element {
 	return (
 		<>
@@ -24,4 +31,22 @@ async function getData(): Promise<Country[]> {
 async function addToIDBStorage(data: Country[]): Promise<void> {
 	await set("countries", data)
 	return
+}
+
+async function updateValues({
+	setCurrencySymbol,
+	setRate,
+	countryCode,
+	karat: selectedKarat,
+}: RenderValue): Promise<void> {
+	let countryData = await getCountryData(countryCode)
+	setCurrencySymbol(countryData.currencySymbol)
+	
+	let DEFAULT_KARAT = 24
+	let rate = countryData.currentPrice / DEFAULT_KARAT
+	rate = rate * parseInt(selectedKarat)
+	
+	let rateString = rate.toFixed(2)
+	rate = parseFloat(rateString)
+	setRate(rate)
 }
