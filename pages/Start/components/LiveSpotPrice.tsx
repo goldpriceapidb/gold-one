fbb4ec694c80fde4a0ff20fc0886af04daa7e945
tharunoptimus <tharunoptimus@outlook.com>
2023-01-31 11:47:16 +0530

    Added `getCountryData`

--- pages/Start/components/LiveSpotPrice.tsx
+++ pages/Start/components/LiveSpotPrice.tsx
@@ -5,3 +5,12 @@ export default function LiveSpotPrice(): JSX.Element {
 	)
 }
 
+
+async function getCountryData(countryCode: string): Promise<Country> {
+	let data: any = await get("countries")
+	if (data === undefined) data = await getData()
+	let countryData = data.filter(
+		(country: Country) => country.countryCode === countryCode
+	)
+	return countryData[0]
+}
