00db7da30df36c2b95af0671f28e487f904fbcf9
tharunoptimus <tharunoptimus@outlook.com>
2023-02-04 18:32:53 +0530

    Title Index with 100% extra code

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -1,14 +1,47 @@
 import styles from "./title.module.css"
 
-export default function Title(prop: any): JSX.Element {
+interface TitleProps {
+    pageTitle: string
+}
+
+export default function Title(prop: TitleProps): JSX.Element {
+    if(!prop.pageTitle) {
+        throw new Error('pageTitle prop is required');
+    }
+    
+    if(typeof prop.pageTitle !== 'string') {
+        throw new Error('pageTitle prop is not a string');
+    }
+    
     return (
-        <>
-            <div className={styles.titleContainer}>
-                <div className={styles.titleText}>
-                    {prop.pageTitle}
-                </div>
-                <div className={styles.seperator} />
-            </div>
-        </>
+        <div className={styles.titleContainer}>
+            {getTitleText(prop.pageTitle)}
+            {getSeperator()}
+        </div>
     )
+}
+
+function getTitleText(pageTitle: string): JSX.Element {
+    if (pageTitle) {
+        return (
+            <div className={styles.titleText}>
+                {pageTitle}
+            </div>
+        )
+    } else {
+        return (
+            <div />
+        )
+    }
+}
+
+function getSeperator(): JSX.Element {
+    try {
+        return (
+            <div className={styles.seperator} />
+        )
+    } catch (ex) {
+        console.log(ex)
+        return <></>
+    }
 }
