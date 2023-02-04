762f15af7664ab1e9f90fc586610747236e8ce0e
KrishnanRGithub <vinokrish001@gmail.com>
2023-02-04 20:34:58 +0530

    added contact page need to link mailto: phoneto:

--- pages/ContactPage/index.tsx
+++ pages/ContactPage/index.tsx
@@ -1,10 +1,15 @@
 import Header from "../../components/Header"
 import style from "./contact.module.css"
+import ContactUs from "./components/ContactUs"
+import Title from "../../components/Title"
+import Footer from "../../components/Footer"
 function ContactPage(): JSX.Element {
     return (
         <>
             <Header />
-
+            <Title pageTitle={"CONTACT US"} />
+            <ContactUs />
+            <Footer />
         </>
     )
 }
--- pages/EnquiryPage/index.tsx
+++ pages/EnquiryPage/index.tsx
@@ -1,9 +1,13 @@
 import Header from "../../components/Header"
 import style from "./enquiry.module.css"
+import Title from "../../components/Title"
+import Footer from "../../components/Footer"
 function EquiryPage(): JSX.Element {
     return (
         <>
             <Header />
+            <Title pageTitle={"EQUIRIES"} />
+            <Footer />
 
         </>
     )
