3cfeb0fa2d035f8b638a5326782ac778a07af330
tharunoptimus <tharunoptimus@outlook.com>
2023-02-04 22:40:17 +0530

    Added `FinalNotes` with its styles

---
+++ pages/OurProducts/components/FinalNotes.tsx
@@ -0,0 +1,78 @@
+import styles from '../products.module.css'
+import { goldBarData, iconsData, SocialMediaProps } from "../data"
+
+export default function FinalNotes(): JSX.Element {
+	return (
+		<div className={styles.finalNotes}>
+			<LeftBox />
+			<RightBox />
+		</div>
+	)
+}
+
+function LeftBox(): JSX.Element {
+	return (
+		<div className={styles.leftBox}>
+			<span className={styles.titleContent}>PLACE YOUR ORDER</span>
+			<HereButton />
+		</div>
+	)
+}
+
+function HereButton(): JSX.Element {
+	return (
+		<div className={styles.hereButton}>
+			<span>
+				<a
+					href="https://google.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					HERE
+				</a>
+			</span>
+			<img
+				src="/socials/select.webp"
+				alt="Select Icon"
+				height={35}
+				width={35}
+			/>
+		</div>
+	)
+}
+
+function RightBox(): JSX.Element {
+	return (
+		<div className={styles.rightBox}>
+			<span className={styles.titleContent}>FIND US ON</span>
+			<SocialIcons />
+		</div>
+	)
+}
+
+function SocialIcons(): JSX.Element {
+	return (
+		<div className={styles.iconsContainer}>
+			{iconsData.map((icon: SocialMediaProps) => {
+				return (
+					<Icon
+						key={icon.key}
+						src={icon.src}
+						alt={icon.alt}
+						url={icon.url}
+					/>
+				)
+			})}
+		</div>
+	)
+}
+
+function Icon(props: any): JSX.Element {
+	return (
+		<div className={styles.iconContainer}>
+			<a href={props.url} target="_blank" rel="noreferrer">
+				<img src={props.src} alt={props.alt} height={50} width={50} />
+			</a>
+		</div>
+	)
+}
