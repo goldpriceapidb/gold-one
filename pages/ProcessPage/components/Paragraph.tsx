bc6883fd7004d1a512d043f3764895000b24f9ec
tharunoptimus <tharunoptimus@outlook.com>
2023-02-04 23:49:49 +0530

    Added Golden Line For the Para titles of `Process`

--- pages/ProcessPage/components/Paragraph.tsx
+++ pages/ProcessPage/components/Paragraph.tsx
@@ -4,10 +4,7 @@ export default function Paragraph(prop: any): JSX.Element {
     return (
         <>
             <div className={styles.paraContainer}>
-                <div className={styles.paraTitleContainer}>
-                    <div className={styles.paraTitle}>{prop.title}</div>
-                    {/* <div className={styles.goldenLine}>.</div> */}
-                </div>
+                <div className={styles.paraTitle}>{prop.title}</div>
                 <div className={styles.paraContent}>{prop.content}</div>
             </div>
         </>
