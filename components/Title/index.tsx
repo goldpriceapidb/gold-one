1c99f5513469d518ff6e83bc60dc49ff6055da5a
tharunoptimus <tharunoptimus@outlook.com>
2023-02-04 18:33:20 +0530

    Nope, Bad idea

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -5,43 +5,14 @@ interface TitleProps {
 }
 
 export default function Title(prop: TitleProps): JSX.Element {
-    if(!prop.pageTitle) {
-        throw new Error('pageTitle prop is required');
-    }
-    
-    if(typeof prop.pageTitle !== 'string') {
-        throw new Error('pageTitle prop is not a string');
-    }
-    
     return (
-        <div className={styles.titleContainer}>
-            {getTitleText(prop.pageTitle)}
-            {getSeperator()}
-        </div>
-    )
-}
-
-function getTitleText(pageTitle: string): JSX.Element {
-    if (pageTitle) {
-        return (
-            <div className={styles.titleText}>
-                {pageTitle}
+        <>
+            <div className={styles.titleContainer}>
+                <div className={styles.titleText}>
+                    {prop.pageTitle}
+                </div>
+                <div className={styles.seperator} />
             </div>
-        )
-    } else {
-        return (
-            <div />
-        )
-    }
-}
-
-function getSeperator(): JSX.Element {
-    try {
-        return (
-            <div className={styles.seperator} />
-        )
-    } catch (ex) {
-        console.log(ex)
-        return <></>
-    }
+        </>
+    )
 }
