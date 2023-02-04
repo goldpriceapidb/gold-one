cfbb1f61187ebe6f2803cd38e0fdc4b245cc22e4
tharunoptimus <tharunoptimus@outlook.com>
2023-02-04 16:09:53 +0530

    Added WhyChooseUs

--- pages/Start/components/WhyChoose.tsx
+++ pages/Start/components/WhyChoose.tsx
@@ -1,22 +1,34 @@
 import styles from "../start.module.css"
+import { Icons } from "../../../types/StartPageTypes"
+import { whyChooseUsIcons } from "../data"
 
 export default function WhyChooseUs(): JSX.Element {
+	return (
+		<>
+			<div className={styles.wrapper}>
+				<span className={styles.headingTitle}>WHY CHOOSE US</span>
 
-	const data = [
-		{ key: 1, icon: "", text: "" },
-		{ key: 2, icon: "", text: "" },
-		{ key: 3, icon: "", text: "" },
-		{ key: 4, icon: "", text: "" },
-	]
-	return (<>
-
-		<div className={styles.wrapper}>
-			<span className={styles.headingTitle}>WHY CHOOSE US</span>
-
-			<div className={styles.formContainer}>
-				Sanple
+				<div className={styles.iconsContainer}>
+					<IconDiv whyChooseUsIcons={whyChooseUsIcons} />
+				</div>
 			</div>
-		</div>
+		</>
+	)
+}
 
-	</>)
+interface IconDivProps {
+	whyChooseUsIcons: Icons[]
+}
+
+function IconDiv({whyChooseUsIcons}: IconDivProps) : JSX.Element {
+	return (
+		<>
+			{whyChooseUsIcons.map((icon: Icons) => (
+				<div className={styles.iconDiv} key={icon.key}>
+					<img src={icon.iconSrc} alt={icon.text} height={60} width={60} />
+					<span dangerouslySetInnerHTML={{ __html: icon.text }} />
+				</div>
+			))}
+		</>
+	)
 }
