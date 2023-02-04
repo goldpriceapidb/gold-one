0dec2e3d7842d5070d6e76abd0c50a00892dfa89
tharunoptimus <tharunoptimus@outlook.com>
2023-02-04 22:40:43 +0530

    Exports `Products`

---
+++ pages/OurProducts/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./products.module.css"
+import Title from "./components/Title"
+import FinalNotes from "./components/FinalNotes"
+import GoldBarsContainer from "./components/GoldBarsContainer"
+import GoldTypeHolder from "./components/GoldTypeHolder"
+
+export default function Products(): JSX.Element {
+	return (
+		<div className={styles.productsContainer}>
+			<Title pageTitle="OUR PRODUCTS" />
+			<GoldTypeHolder />
+			<GoldBarsContainer />
+			<FinalNotes />
+		</div>
+	)
+}
