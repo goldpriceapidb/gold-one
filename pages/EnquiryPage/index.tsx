0252928320a8338d3c445c7ac8cf015131048ae6
KrishnanRGithub <vinokrish001@gmail.com>
2023-02-05 14:02:23 +0530

    form fronted UI done

--- pages/EnquiryPage/index.tsx
+++ pages/EnquiryPage/index.tsx
@@ -11,6 +11,13 @@ function EquiryPage(): JSX.Element {
                 <div className={style.centerTitle}>WE ARE LOOKING TO EXPAND</div>
                 <div>Watch out this space regularly for updates.</div>
             </div>
+            <div className={style.form}>
+                <input type="text" id="name" name="name" placeholder="Name" className={style.formField}></input>
+                <br />
+                <input type="text" id="phone" name="phone" placeholder="Phone" className={style.formField}></input>
+                <br />
+                <button onClick={() => { alert("Submited") }} className={style.formSubmit}>Submit</button>
+            </div>
             <Footer />
 
         </>
