13bd2ccd666d9c7899884ebe7377695bf6fa7243
tharunoptimus <tharunoptimus@outlook.com>
2023-02-06 23:18:38 +0530

    Added FormSubmitToMail capabilities to career and enquiry

--- pages/CareerPage/index.tsx
+++ pages/CareerPage/index.tsx
@@ -2,25 +2,21 @@ import Header from "../../components/Header"
 import style from "./career.module.css"
 import Title from "../../components/Title"
 import Footer from "../../components/Footer"
+import Question from "../../components/FormToMail"
+
 function CareerPage(): JSX.Element {
-    return (
-        <>
-            <Header />
-            <Title pageTitle={"CAREERS"} />
-            <div className={style.centerText}>
-                <div className={style.centerTitle}>WE ARE HIRING</div>
-                <div>Watch out this space regularly for updates.</div>
-            </div>
-            <div className={style.form}>
-                <input type="text" id="name" name="name" placeholder="Name" className={style.formField}></input>
-                <br />
-                <input type="text" id="phone" name="phone" placeholder="Phone" className={style.formField}></input>
-                <br />
-                <button onClick={() => { alert("Submited") }} className={style.formSubmit}>Submit</button>
-            </div>
-            <Footer />
-        </>
-    )
+	return (
+		<>
+			<Header />
+			<Title pageTitle={"CAREERS"} />
+			<div className={style.centerText}>
+				<div className={style.centerTitle}>WE ARE HIRING</div>
+				<div>Watch out this space regularly for updates.</div>
+			</div>
+			<Question type="career" />
+			<Footer />
+		</>
+	)
 }
 
 export default CareerPage
--- pages/EnquiryPage/index.tsx
+++ pages/EnquiryPage/index.tsx
@@ -2,6 +2,8 @@ import Header from "../../components/Header"
 import style from "./enquiry.module.css"
 import Title from "../../components/Title"
 import Footer from "../../components/Footer"
+import Question from "../../components/FormToMail"
+
 function EquiryPage(): JSX.Element {
     return (
         <>
@@ -11,13 +13,7 @@ function EquiryPage(): JSX.Element {
                 <div className={style.centerTitle}>WE ARE LOOKING TO EXPAND</div>
                 <div>Watch out this space regularly for updates.</div>
             </div>
-            <div className={style.form}>
-                <input type="text" id="name" name="name" placeholder="Name" className={style.formField}></input>
-                <br />
-                <input type="text" id="phone" name="phone" placeholder="Phone" className={style.formField}></input>
-                <br />
-                <button onClick={() => { alert("Submited") }} className={style.formSubmit}>Submit</button>
-            </div>
+            <Question type="enquiry" />
             <Footer />
 
         </>
