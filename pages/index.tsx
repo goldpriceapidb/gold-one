754a16516a8a582d66c074d4e6ff2aa951b28d24
tharunoptimus <tharunoptimus@outlook.com>
2023-02-06 23:54:23 +0530

    No separate global.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,6 +43,13 @@ function IndexPage(): JSX.Element {
 					margin: 0;
 					padding: 0;
 					background-color: #000000;
+					-webkit-touch-callout: none; /* iOS Safari */
+					-webkit-user-select: none; /* Safari */
+					-khtml-user-select: none; /* Konqueror HTML */
+					-moz-user-select: none; /* Old versions of Firefox */
+					-ms-user-select: none; /* Internet Explorer/Edge */
+					user-select: none; /* Non-prefixed version, currently
+										  supported by Chrome, Edge, Opera and Firefox */
 				}
 			`}</style>
 		</>
