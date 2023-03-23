085f09ee692cc377f5baca429485621be11e482c
KrishnanRGithub <vinokrish001@gmail.com>
2023-03-23 11:15:15 +0530

    spelling mistake

--- pages/AboutPage/index.tsx
+++ pages/AboutPage/index.tsx
@@ -6,7 +6,7 @@ import Line from "./components/Line"
 function AboutPage(): JSX.Element {
 
     const data = [
-        { key: 1, line: "GGold One Refinery Private Limited was incorporated in 2019 with the goal of providing pure gold bars to businesses and investors. We have a wide variety of sizes and weights to choose from, so you can find the perfect bar for your needs. Our goal is to provide our customers with the best benefits and the highest quality gold possible. We always price our products at the current market rate and follow all industry standards when refining our gold." },
+        { key: 1, line: "Gold One Refinery Private Limited was incorporated in 2019 with the goal of providing pure gold bars to businesses and investors. We have a wide variety of sizes and weights to choose from, so you can find the perfect bar for your needs. Our goal is to provide our customers with the best benefits and the highest quality gold possible. We always price our products at the current market rate and follow all industry standards when refining our gold." },
         { key: 2, line: "Gold One Refinery Private Limited has earned a formal acknowledgement of laboratory proficiency from the NABL in accordance with international standards. This provides our customers and investors with the confidence that they're getting a top-quality product, increasing business on account of improved customer satisfaction. It also helps in saving both time and money as the re-testing of products is no longer necessary." },
         { key: 3, line: "Due to testing by authorized labs and our high-quality finish, Gold One is present as a seller in both domestic and international markets." },
     ]
