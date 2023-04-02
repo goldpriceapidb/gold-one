dc08ef1af8c70195dcd77758e8bad7ff588b4321
tharunoptimus <tharunoptimus@outlook.com>
2023-04-02 21:19:23 +0530

    _app.tsx now handles the global styles

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import "../styles/global.css"
+import { AppProps } from "next/app"
+import "../styles/global.css"
+
+export default function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />
+}
