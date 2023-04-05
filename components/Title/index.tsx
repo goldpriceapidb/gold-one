3028a80791e66e1ee3b33d8794b609c31e483916
tharunoptimus <tharunoptimus@outlook.com>
2023-04-05 09:03:20 +0530

    FadeInUp animation for all titles

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -1,15 +1,17 @@
 import styles from "./title.module.css"
+import animation from "../animation.module.css"
+
 import { TitleProps } from "../../types/GlobalTypes"
 
 export default function Title(prop: TitleProps): JSX.Element {
-    return (
-        <>
-            <div className={styles.titleContainer}>
-                <div className={styles.titleText}>
-                    {prop.pageTitle}
-                </div>
-                <div className={styles.seperator} />
-            </div>
-        </>
-    )
+	let style = `${styles.titleContainer} ${animation.animate__animated} ${animation.animate__fadeInUp}`
+
+	return (
+		<>
+			<div className={style}>
+				<div className={styles.titleText}>{prop.pageTitle}</div>
+				<div className={styles.seperator} />
+			</div>
+		</>
+	)
 }
